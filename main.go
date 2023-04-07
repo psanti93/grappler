f70264981a6ded653556b3b8e75121228ee35485
Paul Santiago <paulsantiago282@gmail.com>
2023-04-07 18:58:40 -0400

    Update parse file

--- main.go
+++ main.go
@@ -1,40 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/psanti93/grappler/controller"
+	"github.com/psanti93/grappler/templates"
 	"github.com/psanti93/grappler/views"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tmplPath)
-
-}
-
-func signUpPage(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "signup.gohtml")
-	executeTemplate(w, tmplPath)
-}
-
-func executeTemplate(w http.ResponseWriter, filePath string) {
-	tmpl, err := views.Parse(filePath)
-
-	if err != nil {
-		fmt.Errorf("There was an issue parsing the file: %v", err)
-		http.Error(w, "Files could not be parsed", http.StatusInternalServerError)
-	}
-
-	tmpl.Execute(w, nil)
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homePage)
-	r.Get("/signUp", signUpPage)
+
+	tmpl := views.Must(views.Parse(templates.FS, "home.gohtml"))
+	r.Get("/", controller.StaticController(tmpl))
+
+	tmpl = views.Must(views.Parse(templates.FS, "signup.gohtml"))
+
+	r.Get("/signup", controller.StaticController(tmpl))
+
+	tmpl = views.Must(views.Parse(templates.FS, "faq.gohtml"))
+	r.Get("/faq", controller.FAQ(tmpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not found", http.StatusNotFound)
 	})
